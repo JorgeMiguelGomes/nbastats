46d815549b5a1cf8305d69d213dacf686005a75c
Jorge Gomes <jorge.gomes@vost.pt>
2022-10-11 11:44:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import dash_bootstrap_components as dbc
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.MINTY]
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
 server = app.server
+
+if __name__ == "__main__":
+    app.run_server(host='0.0.0.0', debug=True)
